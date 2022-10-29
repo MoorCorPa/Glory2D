13b0f70a23f29a87592fdd0ba53bcc73b079d298
Linmo <30459943+MoorCorPa@users.noreply.github.com>
2022-10-29 22:57:59 +0800

    存档

---
+++ Assets/Scripts/Archive/存档.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class 存档
+{
+    public List<GameObject> 怪物 = new List<GameObject>();
+    public GameObject 玩家 = new GameObject();
+}
---
+++ Assets/Scripts/Archive/存档管理器.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class 存档管理器
+{
+    private 存档 创建存档对象()
+    {
+        存档 save = new 存档();
+        
+
+        return save;
+    }
+}
