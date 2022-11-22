92d28a3ca379d93b292281c448017e825941ee38
音理理 <k128667@qq.com>
2022-11-22 15:45:24 +0800

    开始页

--- Assets/Scripts/Home/settingPanle.cs
+++ Assets/Scripts/Home/settingPanle.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.Audio;
 
 public class settingPanle : MonoBehaviour
@@ -13,26 +10,14 @@ public class settingPanle : MonoBehaviour
     {
         gameObject.SetActive(false);
     }
+
     public void 音乐控制(float i)
     {
-        if (i > -30)
-        {
-            音乐.SetFloat("BGMAudioMixer", i);
-        }
-        else
-        {
-            音效.SetFloat("BGMAudioMixer", -80);
-        }
+        音乐.SetFloat("BGMAudioMixer", i > -30 ? i : -80);
     }
+
     public void 音效控制(float i)
     {
-        if (i > -30)
-        {
-            音效.SetFloat("BGMAudioMixer", i);
-        }
-        else
-        {
-            音效.SetFloat("BGMAudioMixer", -80);
-        }
+        音效.SetFloat("BGMAudioMixer", i > -30 ? i : -80);
     }
 }
